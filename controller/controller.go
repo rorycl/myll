e2c4f91f8abd3b2e751eb0199b62735d59ff4786
rorycl <rorycl@campbell-lange.net>
2025-02-16 21:16:18 +0000

    rework for controller; add faq

--- controller/controller.go
+++ controller/controller.go
@@ -20,6 +20,27 @@ func contact(w http.ResponseWriter, r *http.Request) {
 }
 
 func faq(w http.ResponseWriter, r *http.Request) {
+	questions := []struct {
+		Question string
+		Answer   string
+	}{
+		{
+			Question: "Is there a free version?",
+			Answer:   "Yes! We offer a free trial for 30 days on any paid plans.",
+		},
+		{
+			Question: "What are your support hours?",
+			Answer:   "We have support staff answering emails 24/7, though response times may be a bit slower on weekends.",
+		},
+		{
+			Question: "How do I contact support?",
+			Answer:   `Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>`,
+		},
+	}
+	writeTemplate(w, "faq.html", questions)
+}
+
+func faq2(w http.ResponseWriter, r *http.Request) {
 	something := chi.URLParam(r, "something")
 	fmt.Fprintf(w, "faq (with %s)", something)
 }
--- main.go
+++ main.go
@@ -1,54 +1,22 @@
 package main
 
 import (
+	"embed"
 	"fmt"
-	"net/http"
 	"os"
 
+	"mylenslocked/controller"
 	"mylenslocked/views"
-
-	"github.com/go-chi/chi/middleware"
-	"github.com/go-chi/chi/v5"
 )
 
-// global: to replace
-var writeTemplate views.WriteTplFunc
-
-func home(w http.ResponseWriter, r *http.Request) {
-	writeTemplate(w, "home.html", map[string]string{"URL": r.RequestURI})
-}
-
-func contact(w http.ResponseWriter, r *http.Request) {
-	writeTemplate(w, "contact.html", map[string]string{"URL": r.RequestURI})
-}
-
-func faq(w http.ResponseWriter, r *http.Request) {
-	something := chi.URLParam(r, "something")
-	fmt.Fprintf(w, "faq (with %s)", something)
-}
-
-func notFound(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-	writeTemplate(w, "404.html", map[string]string{"URL": r.RequestURI})
-}
+//go:embed views/templates/*html
+var tpls embed.FS
 
 func main() {
-	var err error
-	writeTemplate, err = views.LoadTemplates("views/templates/*html")
+	writeTemplate, err := views.LoadTemplates(tpls, "views/templates", "*html", false)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	// r.Use(middleware.Recoverer)
-	r.Get("/", home)
-	// r.Get("/qaf(/{something})*", faq)
-	r.Get("/faq", faq)
-	r.Get("/faq/", faq)
-	r.Get("/faq/{something}", faq)
-	r.Get("/contact", contact)
-	r.NotFound(notFound)
-	fmt.Println("serving on :3000")
-	http.ListenAndServe(":3000", r)
+	controller.Serve(writeTemplate)
 }
